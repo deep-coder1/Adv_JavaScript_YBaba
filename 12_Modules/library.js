8d602b2ccac3cbd35c8231a224b8d25756dc17ce
deep-coder1 <ndktbd@gmail.com>
2024-10-05 05:44:31 +0530

    12 Modules, 12 Adv JS.

--- 12_Modules/library.js
+++ 12_Modules/library.js
@@ -1,1 +1,50 @@
-export let message = "ES6 Modules";
+// // 1 -->
+// export let message = "ES6 Modules";
+
+// export function user(name){
+//     // console.log(`Hello ${name}`);
+//     return `Hello ${name}`;
+// }
+
+// export class test{
+//     constructor(){
+//         console.log("Constructor Method");
+//     }
+// }
+
+
+// // 2-->
+// let message = "ES6 Modules";
+
+// function user(name){
+//     return `Hello ${name}`;
+// }
+
+// class test{
+//     constructor(){
+//         console.log("Constructor Method");
+//     }
+// }
+
+// export { message, user, test };
+
+// // 3 -->
+// export default function(){
+//     console.log("Hello");
+// }
+
+// let message = "ES6 Modules";
+
+// function user(name){
+//     return `Hello ${name}`;
+// }
+
+// export { message , user };
+
+
+// 4 -->
+function user(){
+    return `Hello`;
+}
+
+export { user };
--- 12_Modules/main.js
+++ 12_Modules/main.js
@@ -1,3 +1,41 @@
-import { message } from "./library.js";
+// // 1 -->
+// // import { message, user, test } from "./library.js";
+// import { message, user as us, test } from "./library.js";
 
-console.log(message);
+// console.log(message);
+
+// // document.body.innerHTML = message;
+
+// // user("Deepak Thakur");
+
+// // console.log(user("Deepak Thakur"));
+// console.log(us("Deepak Thakur"));
+
+// let a = new test();
+
+
+// // 2 -->
+// import * as deepak from "./library.js";
+
+// console.log(deepak.message);
+
+// console.log(deepak.user("Deepak Thakur"));
+
+// let a = new deepak.test();
+
+// // 3 -->
+// // import { default as deepak } from "./library.js";
+// import deepak from "./library.js";
+// import {message, user} from "./library.js";
+
+// deepak();
+
+// console.log(message);
+
+// console.log(user("Deepak Thakur"));
+
+
+// 4 -->
+import { user } from "./bridge.js";
+
+console.log(user());
